2d6aeeba252a9785d7f870f925993842c0f7b8b6
Shah Limon <limon.working@gmail.com>
2023-10-24 08:51:43 +0600

    update orders menu

--- src/Pages/Admin/OrderMenu.js
+++ src/Pages/Admin/OrderMenu.js
@@ -4,37 +4,47 @@ import { Link } from "react-router-dom";
 const OrderMenu = () => {
   return (
     <div>
-      <section className="project s2">
-        <div className="shape right" />
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-3 col-md-6">
-              <Link to="/admin/orders/pending" class="action-btn ">
-                <span> Pending Orders</span>
+      <div className="header__right container custom-orders">
+        <nav id="main-nav" className="main-nav">
+          <ul id="menu-primary-menu" className="menu custom-orders-ul">
+            <li className="menu-item menu-item-has-children">
+              <Link to="/admin/orders/pending" class="action-btn">
+                Pending Orders
               </Link>
-            </div>
-            <div className="col-xl-3 col-md-6">
-              <div className="project-box">
-                <div className="image"></div>
-                <div className="content">
-                  <Link to="/admin/orders/accepted" class="action-btn ">
-                    Accepted Orders
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-3 col-md-6">
-              <div className="project-box">
-                <div className="image"></div>
-                <div className="content">
-                  
-                  
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+            </li>
+
+            <li className="menu-item menu-item-has-children">
+              <Link to="/admin/payments/pending" class="action-btn ">
+                Pending Payments
+              </Link>
+            </li>
+
+            <li className="menu-item">
+              <Link to="/admin/orders/accepted" class="action-btn ">
+                Accepted Orders
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/admin/payments/received" class="action-btn ">
+                Recived Payments
+              </Link>
+            </li>
+
+            <li className="menu-item">
+              <Link to="/admin/orders/cancelled" class="action-btn ">
+                Cancelled orders
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/admin/payments/cancelled" class="action-btn ">
+                Cancelled Payments
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+     
     </div>
   );
 };
